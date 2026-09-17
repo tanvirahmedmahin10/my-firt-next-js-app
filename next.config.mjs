d9bfec887cecf62bc6cd4d568b220b34794d8de6
tanvirahmedmahin10 <tanvirahmedmahinsarker@gmail.com>
2026-09-17 23:21:14 +0600

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  //"https://content.screen.nsw.gov.au/assets/Uploads/Past-productions/valorant.webp"
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**',
+        search: '',
+      },
+       {
+        protocol: 'http',
+        hostname: '**',
+        port: '',
+        pathname: '**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 
 
 const page = () => {
     return (
         <div>
            <h2>putki marmu tore</h2> 
+            <Image
+      src="https://content.screen.nsw.gov.au/assets/Uploads/Past-productions/valorant.webp"
+      alt="Valorant"
+      width={500}
+      height={500}
+    />
         </div>
     );
 };
